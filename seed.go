086da0482f4f522aaf4ffc96ef7bfbd762eef7c9
lrf141 <ghost141.kentyo@gmail.com>
2019-10-26 02:23:51 +0900

    fix go fmt

--- seed.go
+++ seed.go
@@ -1,61 +1,59 @@
 package main
 
 import (
-        "fmt"
-        "github.com/urfave/cli"
-        "time"
+	"fmt"
+	"github.com/urfave/cli"
+	"time"
 )
 
 const seedDir = "./migrations/seed"
 
 func seedRunAction(c *cli.Context) {
 
-        db := prepareDbDriver()
-        fmt.Println(db.Driver)
+	db := prepareDbDriver()
+	fmt.Println(db.Driver)
 
-        defer func() {
-                err := db.Driver.Close()
-                if err != nil {
-                        panic(err.Error())
-                }
-        }()
+	defer func() {
+		err := db.Driver.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}()
 }
-
 
 func seedCreateAction(c *cli.Context) error {
 
-        if c.NumFlags() < 1 || fileName == "" {
-                err := cli.ShowCommandHelp(c, "create:seed")
-                if err != nil {
-                        panic(err.Error())
-                }
-                return cli.NewExitError("Please set seeds name.", 1)
-        }
+	if c.NumFlags() < 1 || fileName == "" {
+		err := cli.ShowCommandHelp(c, "create:seed")
+		if err != nil {
+			panic(err.Error())
+		}
+		return cli.NewExitError("Please set seeds name.", 1)
+	}
 
-        if !isDirExist(seedDir) {
-                err := mkDir(seedDir)
-                if err != nil {
-                        panic(err.Error())
-                }
-        }
+	if !isDirExist(seedDir) {
+		err := mkDir(seedDir)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 
+	t := time.Now().Format("20060102150405")
+	seedFile := fmt.Sprintf("%s/%s_%s", seedDir, t, fileName)
 
-        t := time.Now().Format("20060102150405")
-        seedFile := fmt.Sprintf("%s/%s_%s", seedDir, t, fileName)
+	err := touchFile(seedFile + upSql)
+	if err != nil {
+		panic(err.Error())
+	}
 
-        err := touchFile(seedFile+upSql)
-        if err != nil {
-                panic(err.Error())
-        }
+	err = touchFile(seedFile + downSql)
+	if err != nil {
+		err2 := deleteFile(seedFile + upSql)
+		if err2 != nil {
+			panic(err2.Error())
+		}
+		panic(err.Error())
+	}
 
-        err = touchFile(seedFile+downSql)
-        if err != nil {
-                err2 := deleteFile(seedFile+upSql)
-                if err2 != nil {
-                        panic(err2.Error())
-                }
-                panic(err.Error())
-        }
-
-        return nil
+	return nil
 }
--- util.go
+++ util.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-        "fmt"
-        "os"
+	"fmt"
+	"os"
 )
 
 const (
-        upSql = ".up.sql"
-        downSql = ".down.sql"
+	upSql   = ".up.sql"
+	downSql = ".down.sql"
 )
 
 func isDirExist(dirName string) bool {
-        _, err := os.Stat(dirName)
-        if err != nil {
-                return false
-        }
-        return true
+	_, err := os.Stat(dirName)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func mkDir(name string) error {
-        return os.Mkdir(name, 0777)
+	return os.Mkdir(name, 0777)
 }
 
 func isFileExist(fileName string) bool {
-        _, err := os.Stat(fileName)
-        if err != nil {
-                return false
-        }
-        return true
+	_, err := os.Stat(fileName)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func touchFile(name string) error {
 
-        file, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE, 0777)
-        if err != nil {
-                panic(err.Error())
-        }
+	file, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		panic(err.Error())
+	}
 
-        defer func() {
-                err := file.Close()
-                if err != nil {
-                        panic(err.Error())
-                }
-        }()
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}()
 
-        _, err = fmt.Fprintln(file, "")
-        return err
+	_, err = fmt.Fprintln(file, "")
+	return err
 }
 
 func deleteFile(name string) error {
-        return os.Remove(name)
+	return os.Remove(name)
 }
